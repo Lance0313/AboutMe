b73ee3ed4c5a8ef739416d1b996a0a58ee8b3e93
Lance0313 <lncdlrsr@gmail.com>
2024-06-22 19:36:18 +0800

    Update

---
+++ main.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+    data() {
+        return {
+            name: 'Lance G. Del Rosario',
+            degree: 'BS in Computer Engineering'
+        };
+    }
+}).mount('#profileName');
